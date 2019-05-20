15c2c822719a88a582c535298de2e0f7d4f8f2b0
shinvi <378042176@qq.com>
2019-05-21 00:39:55 +0800

    添加Cookie-Based方式认证

---
+++ MvcCookiesAuthSample/MvcCookiesAuthSample/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcCookiesAuthSample.Controllers
+{
+
+    public class AccountController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult MakeLogin()
+        {
+            var claims = new List<Claim>() {
+                new Claim(ClaimTypes.Name,"jgp"),
+                new Claim(ClaimTypes.Role,"admin")
+            };
+            var claimIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,new ClaimsPrincipal(claimIdentity));
+            return Ok();
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Ok();
+        }
+    }
+}
---
+++ MvcCookiesAuthSample/MvcCookiesAuthSample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcCookiesAuthSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
